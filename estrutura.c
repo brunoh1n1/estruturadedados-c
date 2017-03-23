8f8d24c1bbb2324db3d19efe2896bf3f6dc67513
root <root@H1N1.localdomain>
2017-03-23 00:57:57 -0300

    Primeiro programa em C

---
+++ estrutura.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main () {
+
+int a, b, c, media;
+
+
+printf ("Informe o primeiro valor: ");
+scanf("%d", &a);
+
+printf ("Informe o segundo valor: ");
+scanf("%d", &b);
+
+printf("Informe o terceiro valor: ");
+scanf("%d", &c);
+
+media = (a + b + c)/3;
+
+printf ("Resultado media %di\n\n ", media);
+
+return 0;
+}
+
